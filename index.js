eb34789d8275d7967233c6d527353ea8f55d3f32
shishiro <bannu1302@gmail.com>
2024-11-20 12:38:11 +0530

    added hte morgan

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import morgan from "morgan";
 dotenv.config();
 
 const port = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(morgan("dev"));
 
 mongoose
   .connect(process.env.MONGO_URI, {
